d3d8384602c48675cb13638b57b24dfc94c756b5
Kirill Safonov <kirsafonov@gmail.com>
2026-06-04 18:14:03 +0300

    Add Yandex Metrika counter

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
+import Script from "next/script";
 import type { Metadata } from "next";
 import "./globals.css";
 import { JsonLd } from "@/components/seo/JsonLd";
 import { getOrganizationJsonLd, getWebSiteJsonLd } from "@/lib/jsonLd";
+
+const yandexMetrikaId = process.env.NEXT_PUBLIC_YANDEX_METRIKA_ID;
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://stolica-msk.com"),
@@ -33,6 +36,41 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <body>
+        {yandexMetrikaId ? (
+          <>
+            <Script id="yandex-metrika" strategy="afterInteractive">
+              {`
+                (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {
+                    if (document.scripts[j].src === r) { return; }
+                  }
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+                  k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+                })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
+
+                ym(${yandexMetrikaId}, 'init', {
+                  ssr: true,
+                  webvisor: true,
+                  clickmap: true,
+                  trackLinks: true,
+                  accurateTrackBounce: true
+                });
+              `}
+            </Script>
+
+            <noscript>
+              <div>
+                <img
+                  src={`https://mc.yandex.ru/watch/${yandexMetrikaId}`}
+                  style={{ position: "absolute", left: "-9999px" }}
+                  alt=""
+                />
+              </div>
+            </noscript>
+          </>
+        ) : null}
         <JsonLd data={[getOrganizationJsonLd(), getWebSiteJsonLd()]} />
         {children}
       </body>
