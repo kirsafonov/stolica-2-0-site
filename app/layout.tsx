dc1bb808a6aaaa9bd1ddf26599761009b301af20
Kirill Safonov <kirsafonov@gmail.com>
2026-06-04 18:44:18 +0300

    Change png to ico favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon.ico",
-    apple: "/favicon.png",
+    apple: "/favicon.ico",
   },
   formatDetection: {
     telephone: false,
